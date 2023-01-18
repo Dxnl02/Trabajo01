42070de4a0ae6196fe0797cace0e341ce2296913
sanzapedro7 <sanzapedro7@gmail.com>
2023-01-18 11:09:03 +0100

    puntos

--- Assets/ScriptsPersonaje/GameManager.cs
+++ Assets/ScriptsPersonaje/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] public int puntuacionActual, mejorPuntuacion;
     [SerializeField] float tiempo;
     [SerializeField] TMP_Text textoTiempo;
+    [SerializeField] GameObject texto, boton;
+    [SerializeField] GameObject jugador, enemigo;
+    [SerializeField] bool actdescronometro;
 
     public static GameManager Instance { get; internal set; }
 
@@ -15,6 +18,11 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         Instance = this;
+    }
+
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
@@ -29,12 +37,21 @@ public class GameManager : MonoBehaviour
 
     public void Perder()
     {
-
+            jugador.SetActive(false);
+            enemigo.SetActive(false);
+            boton.SetActive(true);
+            texto.SetActive(true);
+            actdescronometro = false;
     }
 
     public void ReiniciarJuego()
     {
-
+        puntuacionActual = 0;
+        jugador.SetActive(true);
+        enemigo.SetActive(true);
+        tiempo = 0;
+        actdescronometro = true;
+        enemigo.GetComponent<Enemigo>().IniciarEnemigo();
     }
 
     public void ActualizarPuntuacion()
